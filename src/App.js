cd34d6bd51a6425fc365b678aceb09d4d54cf927
Herzenpolo <herzencc@icloud.com>
2020-10-19 15:28:29 -0400

    first real commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,62 @@
-import React from 'react';
+import React, { Component } from "react";
 import logo from './logo.svg';
 import './App.css';
+import Home from './components/Home'
+import Privacy from './components/Privacy'
+import Terms from './components/Terms'
+import { Switch, Link, Route } from 'react-router-dom'
+import Navbar from './components/Navbar'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+class App extends Component { 
+
+  render() {
+    console.log(this.state);
+    return (
+      <div>
+       <div className="App">
+     <Navbar/>
+<Switch>
+ <Route
+          exact
+          path="/"
+          render={(props) => (
+            <Home
+              {...props}
+            />
+          )}
+        />
+        <Route
+          exact
+          path="/privacy"
+          render={(props) => (
+            <Privacy
+              {...props}
+            />
+          )}
+        />
+        <Route
+          exact
+          path="/terms"
+          render={(props) => (
+            <Terms
+              {...props}
+            />
+          )}
+        />
+        <Route
+          exact
+          path="/aboutus"
+          render={(props) => (
+            <Terms
+              {...props}
+            />
+          )}
+        />
+      </Switch>
     </div>
-  );
+      </div>
+    );
+  }
 }
-
 export default App;
