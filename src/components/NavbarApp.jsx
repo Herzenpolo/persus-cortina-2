6763b3a89b864ef52114a777afd63fceda7ff46f
Herzenpolo <herzencc@icloud.com>
2020-10-20 15:22:47 -0400

    navbar

--- src/components/NavbarApp.jsx
+++ src/components/NavbarApp.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import {Link} from 'react-router-dom'
-import { Nav, Button, NavLink } from 'reactstrap';
+
 
 
 
@@ -12,12 +12,12 @@ export default class NavbarApp extends Component {
     render() {
         return (
             <div>
-                <Nav className = 'navBarContainer'>
+                <div className = 'navBarContainer'>
                     <Link className = 'navLink' to="/">Home</Link> 
                     {/* <NavLink className = 'navLink' href="/aboutus">About Us</NavLink>  */}
                     <Link className = 'navLink' to="/privacy">Privacy Policy</Link> 
                     <Link className = 'navLink' to="/terms">Terms & Conditions</Link>
-                </Nav>
+                </div>
             </div>
         )
     }
